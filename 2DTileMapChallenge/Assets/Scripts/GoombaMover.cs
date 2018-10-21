b2a2ea2c164a7cd150614ccdd2d23c99093cec7d
KierstenS12 <43118302+KierstenS12@users.noreply.github.com>
2018-10-21 03:00:24 -0400

    Update 4

---
+++ 2DTileMapChallenge/Assets/Scripts/GoombaMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaMover : MonoBehaviour
+{
+    public float speed;
+    private GameObject wallHitBox;
+    public float wallHitWidth;
+    public float wallHitHeight;
+    private bool wallHit;
+    public Transform wallhitbox;
+    public float checkRadius;
+    public LayerMask isGround;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+        
+    void FixedUpdate()
+    {
+        wallHit = Physics2D.OverlapBox(wallhitbox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {  
+        if (wallHit == true)
+        {
+            speed = speed* -1;
+        }
+    }
+}
+
